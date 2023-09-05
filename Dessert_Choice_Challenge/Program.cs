c85ad42bd60111824104983b3e968cee05800ded
YousufR8 <Yrehman06@gmail.com>
2023-09-05 12:54:15 -0400

    Add project files.

---
+++ Dessert_Choice_Challenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dessert_Choice_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like some dessert?");
+            string dessert = (Console.ReadLine());
+            if (dessert.Contains("yes") == true)
+            {
+                Console.WriteLine("Awesome. I'll grab you a dessert menu");
+                Console.WriteLine("Here are this option: Rose water rice pudding, Tres Lech cakes, Kulfi, Bread pudding, Beignets");
+                string Side = (Console.ReadLine());
+                Console.WriteLine($"I will grab {Side} right away");
+            }
+            else if (dessert.Contains("no") == true)
+            {
+                Console.WriteLine("No Worries. I'll grab you the check");
+             }
+
+            Console.ReadKey();
+
+        }
+    }
+}
